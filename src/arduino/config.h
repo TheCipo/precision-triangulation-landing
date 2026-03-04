0d12cb11eea7d1859be36d01f0712196febe916e
TheCipo <giulio.cipollone@gmail.com>
2026-03-04 20:33:54 +0100

    Add initial source files for Arduino project structure

---
+++ src/arduino/config.h
@@ -0,0 +1,13 @@
+#define MYADDRESS 10 // da cambiare con numero dell'arduino, per ordine non funzionalità
+#define ND -1
+
+#define DATATIMES 21 //numero di volte che vengono raccolti i dati di ogni sensore, meglio dispari
+
+// EEPROM data
+#define EPR_Ax 0
+#define EPR_Ay 4
+#define EPR_Bx 8
+#define EPR_By 12
+#define EPR_Cx 16
+#define EPR_Cy 20
+#define EPR_Max 24
