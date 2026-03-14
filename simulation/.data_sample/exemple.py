78122199ac0be645ba542bd8e5e9503d6fe3319c
TheCipo <giulio.cipollone@gmail.com>
2026-03-14 10:16:30 +0100

    Update .gitignore to include simulation data files and modify data generation script for new output format

--- simulation/.data_sample/exemple.py
+++ simulation/.data_sample/exemple.py
@@ -1,15 +1,34 @@
 #code to generate random data for the 3 sensors of the project and test if the code works
 import random
 import math
+from pathlib import Path
+import csv
+
+n = int(input("Enter the number of elements for the: "))
+R = float(input("Radius = "))
+sensor = [[0, R], [R * math.sqrt(3) / 2.0, -R / 2.0], [-R * math.sqrt(3) / 2.0, -R / 2.0]]
 
 dataFile = open("simulation/.data_sample/sample.csv", "w")
 dataFile.write("s1,s2,s3,x,y\n")
 
-n = int(input("Enter the number of elements for the: "))
-sensor = [[0, 0], [0, 0], [0, 0]]
+lines = [
+        "#ifndef SAMPLE_DATA_H",
+        "#define SAMPLE_DATA_H",
+        "",
+        "#include <stdint.h>",
+        "",
+        f"static const uint16_t SAMPLE_COUNT = {n};",
+        "static const int16_t SAMPLE_DATA[][3] = {",
+    ]
+closeLines = [
+        "};",
+        "",
+        "#endif",
+        "",
+    ]
 
-R = float(input("Radius = "))
-sensor = [[0, R], [R * math.sqrt(3) / 2.0, -R / 2.0], [-R * math.sqrt(3) / 2.0, -R / 2.0]]
+dataLib = open("simulation/esp32/chips/sample_data.h", "w")
+dataLib.write("\n".join(lines))
 
 for i in range(n):
     x = random.randint(-100, 100)
@@ -18,4 +37,6 @@ for i in range(n):
     s2 = round(math.sqrt((x - sensor[1][0])**2 + (y - sensor[1][1])**2))
     s3 = round(math.sqrt((x - sensor[2][0])**2 + (y - sensor[2][1])**2))
     dataFile.write(f"{s1},{s2},{s3},{x},{y}\n")
+    dataLib.write(f"  {{{s1}, {s2}, {s3}}},\n")
 
+dataLib.write("\n".join(closeLines))
