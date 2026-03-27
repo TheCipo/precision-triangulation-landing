81fc879792525137563a01ae7bf1155b101660ce
TheCipo <giulio.cipollone@gmail.com>
2026-03-27 18:04:04 +0100

    Implement degraded mode functionality and vector calculation for drone movement

--- src/esp32/centering_control.cpp
+++ src/esp32/centering_control.cpp
@@ -1,6 +1,25 @@
 #include "centering_control.h"
 #include "drone.h"
+#include <Tello.h>
 
 void degradedMode(int distance, int index) {
-  // TODO: Implementazione della modalità degradata
+  serial.print("Degraded mode!");
+  int deg = 0;
+  switch(index) {
+    case 0:
+      deg = 0;
+      break;
+    case 1:
+      deg = 120;
+      break;
+    case 2:
+      deg = 240;
+      break;
+    default:
+      return; // se l'indice non è valido esce
+  }
+  //calcola il vettore di movimento in base alla distanza e all'angolo
+  float x = distance * cos(deg * PI / 180);
+  float y = distance * sin(deg * PI / 180);
+  vectorToTello(x, y);
 }
--- src/esp32/drone.cpp
+++ src/esp32/drone.cpp
@@ -1,4 +1,20 @@
 #include "drone.h"
+#include <Tello.h>
+
+Tello drone;
+
+void vectorToTello(int x, int y){
+  if (x > 0) { // se x è positivo, muovi a destra
+    drone.right(x);
+  } else if (x < 0) { // se x è negativo, muovi a sinistra
+    drone.left(-x);
+  }
+  if (y > 0) { // se y è positivo, muovi in avanti
+    drone.forward(y);
+  } else if (y < 0) { // se y è negativo, muovi indietro
+    drone.backward(-y);
+  }
+}
 
 void resetTello() {
   //TODO: implementare reset del drone
